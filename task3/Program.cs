98e04b829c79996620fb8836553e83416529a92b
Tatyana Sidorova <tasidorowa@gmail.com>
2024-03-09 19:26:54 +0300

    Задание 3

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Считать строку с консоли, состоящую из латинских букв в нижнем регистре. 
+// Выяснить, сколько среди введённых букв гласных.
+
+int CountVowels(string str)
+{
+    int count = 0;
+    string vowels = "aeiouy";
+    foreach (char item in str.ToLower())
+    {
+        if (vowels.Contains(item))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string str = "Test string";
+int count = CountVowels(str);
+Console.WriteLine(count);
