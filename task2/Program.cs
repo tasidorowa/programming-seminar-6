230277822ec38ab928a08ae21b916e198896add1
Tatyana Sidorova <tasidorowa@gmail.com>
2024-03-09 19:29:09 +0300

    Задание 2

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Сформировать из строки массив символов и вывести его на экран
+
+char[] GetCharsFromString(string str)
+{
+    char[] charsArray = new char[str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+        charsArray[i] = str[i];
+    }
+    return charsArray;
+}
+
+void PrintArray(char[] array)
+{
+    foreach (char item in array)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+string str = "Hello";
+char[] array = GetCharsFromString(str);
+PrintArray(array);
